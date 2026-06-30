effa7622ae0368c29486de35af31689aea6b7f07
Ethan Davis <89031823+davisethan@users.noreply.github.com>
2026-06-30 15:51:28 -0700

    Ported tombolo plots (#8)

---
+++ moabbr/plots/__init__.py
@@ -0,0 +1,17 @@
+from tombolo.plots import (
+    league_table,
+    forest_plot,
+    ranking_plot,
+    heterogeneity_table,
+    prediction_table,
+    convergence_table,
+)
+
+__all__ = [
+    league_table.__name__,
+    forest_plot.__name__,
+    ranking_plot.__name__,
+    heterogeneity_table.__name__,
+    prediction_table.__name__,
+    convergence_table.__name__,
+]
